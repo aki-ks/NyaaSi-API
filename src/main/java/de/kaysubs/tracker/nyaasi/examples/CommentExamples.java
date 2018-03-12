06c8e4c069f5e7f29ffdc791cffc5377177317aa
aki <aki@kaysubs.de>
2018-03-12 14:40:25 +0100

    Added editComment example

--- src/main/java/de/kaysubs/tracker/nyaasi/examples/CommentExamples.java
+++ src/main/java/de/kaysubs/tracker/nyaasi/examples/CommentExamples.java
@@ -9,10 +9,14 @@ public class CommentExamples {
     public static void main(String[] args) {
         NyaaSiAuthApi api = LoginExample.login();
         Scanner sc = new Scanner(System.in);
+        System.out.print("torrent id: ");
         int torrentId = sc.nextInt();
 
         int commentId = api.writeComment(torrentId, "I'm testing a nyaa.si api");
         System.out.println("Example comment has id " + commentId);
+
+        api.editComment(torrentId, commentId, "I'm an edited comment.");
+        System.out.println("The comment has been edited");
 
         api.deleteComment(torrentId, commentId);
         System.out.println("Deleted example comment again");
